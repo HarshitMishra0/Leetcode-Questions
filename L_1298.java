9a72d6638cd4dabbd42f7e4c26c14595ead215cb
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-13 20:37:47 +0530

    All tests passed

---
+++ L_1298.java
@@ -0,0 +1,21 @@
+public class L_1298 {
+    public int findNumbers(int[] nums) {
+
+        int counter = 0;
+
+        for(int i=0; i<nums.length; i++){
+            int n = nums[i];
+            int count = 0;
+            while(n>0){
+                n = n/10;
+                count ++;
+            }
+            if(count!=0 && count%2==0){
+                counter = counter + 1;
+            }
+        }
+        return counter;
+        
+    }
+    
+}
