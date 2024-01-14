41ebafa68880423eaac22e7813ba7172737bd8d8
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-14 10:19:42 +0530

    All tests passed

---
+++ L_125.java
@@ -0,0 +1,18 @@
+public class L_125 {
+    public boolean isPalindrome(String s) {
+         s = s.toLowerCase();
+         s = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        int start = 0;
+        int end =s.length()-1;
+
+        while(start<=end){
+            if(s.charAt(start)!= s.charAt(end)){
+                return false;
+            }
+            start = start + 1;
+            end = end -1;
+        }
+        return true;
+    }
+}
