6fd63ee449a20353344dd5533288d59ba36eace7
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-22 23:16:03 +0530

    All tests pass

---
+++ L_287.java
@@ -0,0 +1,27 @@
+public class L_287 {
+    public int findDuplicate(int[] nums) {
+        int answer = 0;
+
+        int i =0;
+        while (i <nums.length){
+             if(nums[i]!= i+1){
+                 int correct = nums[i] -1;
+            if(nums[i]!=nums[correct]  ){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else{
+                answer = nums[i];
+                return answer;
+                
+            }
+             }else{
+                 i++;
+             }
+            
+        }
+
+        return answer;
+        
+    }
+}
