1c1acd3e542eab51c62ed45ad0ca546e56cf7e98
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-22 23:20:02 +0530

    All tests pass

---
+++ L_1678.java
@@ -0,0 +1,10 @@
+public class L_1678 {
+    public String interpret(String command) {
+        
+        command = command.replace("()", "o");
+        command = command.replace("(al)", "al");
+
+        return command;
+
+    }
+}
