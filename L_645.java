1b7dbe5bea6c0f1e2d437c2e84db3fe0571c95fc
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-23 22:20:54 +0530

    All tests pass

---
+++ L_645.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class L_645 {
+    public int[] findErrorNums(int[] nums) {
+        int i =0;
+
+        while(i <nums.length){
+            int correct = nums[i] -1;
+            if(nums[i]!=nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else{
+                i++;
+            }
+        }
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int j=0; j<nums.length; j++){
+            if(nums[j]!=j+1){
+                arr.add(nums[j]);
+                arr.add(j+1);
+            }
+        }
+
+         int[] result = new int[arr.size()];
+        for (int j = 0; j < arr.size(); j++) {
+            result[j] = arr.get(j);
+        }
+        return result;
+    }
+    
+}
