e60248c996cdd96b833b4795ad8654403363e49c
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-01-23 22:19:44 +0530

    All tests pass

---
+++ L_41.java
@@ -0,0 +1,29 @@
+public class L_41 {
+    public int firstMissingPositive(int[] nums) {
+        
+        int i =0;
+        int n = nums.length;
+        int answer = n;
+
+        while(i<nums.length){
+            int correct = nums[i] -1;
+            if(nums[i]>0 && nums[i]<=nums.length && nums[i]!= nums[correct]   ){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else{
+                i++;
+            }
+        }
+
+        for(int j=0; j<nums.length; j++){
+            if(nums[j]!=j+1){
+                answer = j+1;
+                return answer;
+               
+            }
+        }
+        return n+1;
+    }
+    
+}
