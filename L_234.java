21b79fc7abe957e764a8075b1b42969695d87d07
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-12 22:20:40 +0530

    All tests Passed

--- L_234.java
+++ L_234.java
@@ -1,3 +1,32 @@
 public class L_234 {
+    public boolean isPalindrome(ListNode head) {
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode curr = slow;
+        ListNode prev = null;
+        ListNode next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode firstHalf = head;
+
+        while (prev != null) {
+            if (prev.val != firstHalf.val) {
+                return false;
+            }
+            prev = prev.next;
+            firstHalf = firstHalf.next;
+        }
+        return true;
+    }
 }
