5ac46e3de20eebe2ff7cafa50fce758de6dba7ab
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-17 23:39:06 +0530

    All tests passed

--- L_43.java
+++ L_43.java
@@ -1,3 +1,12 @@
-public class L_43 {
+import java.math.BigInteger;
 
+public class L_43 {
+    public String multiply(String num1, String num2) {
+
+        BigInteger bigInt1 = new BigInteger(num1);
+        BigInteger bigInt2 = new BigInteger(num2);
+
+        BigInteger answer = bigInt1.multiply(bigInt2);
+        return String.valueOf(answer);
+    }
 }
