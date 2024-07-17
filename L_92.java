35b35060f8b0920bbd3592467de8c3118cda0004
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-17 23:40:11 +0530

    All tests passed

--- L_92.java
+++ L_92.java
@@ -1,3 +1,33 @@
 public class L_92 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode current = head;
+        ListNode prev = null;
 
+        if (head == null || left == right)
+            return head;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        ListNode last = prev;
+        ListNode newEnd = current;
+        ListNode next;
+
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = current;
+        return head;
+    }
 }
