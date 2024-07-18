a05fa5ea4f8df63828e9263fb0ba88d1de5e0735
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-18 22:00:01 +0530

    All tests passed

--- L_143.java
+++ L_143.java
@@ -1,3 +1,37 @@
 public class L_143 {
+    public void reorderList(ListNode head) {
 
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = reverse(slow.next);
+        slow.next = null;
+
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            ListNode temp1 = firstHalf.next;
+            ListNode temp2 = secondHalf.next;
+
+            firstHalf.next = secondHalf;
+            secondHalf.next = temp1;
+
+            firstHalf = temp1;
+            secondHalf = temp2;
+        }
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
 }
