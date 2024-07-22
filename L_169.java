e5ce26682ca891caee0b279a2756da22d5e459d0
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-22 21:29:44 +0530

    All tests passed

--- L_169.java
+++ L_169.java
@@ -1,3 +1,8 @@
-public class L_169 {
+import java.util.Arrays;
 
+public class L_169 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
 }
