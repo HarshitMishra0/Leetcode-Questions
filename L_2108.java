100f7cb18c16e44788b9ae90a0370bfdcfe0e2d1
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-22 21:28:44 +0530

    All tests passed

--- L_2108.java
+++ L_2108.java
@@ -1,3 +1,26 @@
 public class L_2108 {
+    public String firstPalindrome(String[] words) {
+        String answer = "";
 
+        for (int i = 0; i < words.length; i++) {
+            boolean palindrome = palindrom(words[i]);
+            if (palindrome == true) {
+                return words[i];
+            }
+        }
+        return answer;
+    }
+
+    public boolean palindrom(String word) {
+        int i = 0;
+        int j = word.length() - 1;
+        while (i < j) {
+            if (word.charAt(i) != word.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
