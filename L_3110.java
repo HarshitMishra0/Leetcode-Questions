b4ead599526d450ae8627972d09e31940a6768ef
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-23 17:07:21 +0530

    All tests passed

--- L_3110.java
+++ L_3110.java
@@ -1,3 +1,10 @@
 public class L_3110 {
+    public int scoreOfString(String s) {
 
+        int answer = 0;
+        for (int i = 1; i < s.length(); i++) {
+            answer = answer + Math.abs(s.charAt(i) - s.charAt(i - 1));
+        }
+        return answer;
+    }
 }
