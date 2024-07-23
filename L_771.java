ecd99236a8efd7da6b691d179f5531aa708b25e4
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-23 17:05:41 +0530

    All tests passed

--- L_771.java
+++ L_771.java
@@ -1,3 +1,23 @@
 public class L_771 {
+    public int numJewelsInStones(String jewels, String stones) {
+        int answer = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            char a = jewels.charAt(i);
+            String b = "" + a;
+            if (stones.contains(b)) {
+                answer = answer + find(stones, a);
+            }
+        }
+        return answer;
+    }
 
+    public int find(String stones, char a) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (stones.charAt(i) == a) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
