74e41c2cfb0455872d31b173d55c1069759b8835
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-24 08:49:19 +0530

    All tests passed

--- L_2562.java
+++ L_2562.java
@@ -1,3 +1,20 @@
 public class L_2562 {
+    public long findTheArrayConcVal(int[] nums) {
+        long answer = 0;
+        int j = nums.length - 1;
 
+        for (int i = 0; i < nums.length / 2; i++) {
+            if (i != j) {
+                String yes = "" + nums[i] + nums[j];
+                answer = answer + Integer.parseInt(yes);
+                j--;
+            }
+        }
+
+        if (nums.length % 2 != 0) {
+            String yes = "" + nums[j];
+            answer = answer + Integer.parseInt(yes);
+        }
+        return answer;
+    }
 }
