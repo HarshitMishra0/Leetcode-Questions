9a0f94842a1941566fd4bb601b048c4111df88ba
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-07-29 06:29:06 +0530

    All tests passed

--- L_232.java
+++ L_232.java
@@ -1,3 +1,41 @@
-public class L_232 {
+import java.util.Stack;
 
+public class L_232 {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+
+    public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void push(int x) {
+        first.add(x);
+    }
+
+    public int pop() {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+        int answer = second.pop();
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+        return answer;
+    }
+
+    public int peek() {
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+        int answer = second.peek();
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+        return answer;
+    }
+
+    public boolean empty() {
+        return first.isEmpty() && second.isEmpty();
+    }
 }
