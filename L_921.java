cc16b918dced8c3d68e8e0af9280a946402f02ef
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-01 06:22:40 +0530

    All tests passed

--- L_921.java
+++ L_921.java
@@ -1,3 +1,21 @@
-public class L_921 {
+import java.util.Stack;
 
+public class L_921 {
+    public int minAddToMakeValid(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(')');
+                }
+            } else {
+                stack.push('(');
+            }
+        }
+        return stack.size();
+    }
 }
