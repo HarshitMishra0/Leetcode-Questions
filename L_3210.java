a98e2664f7d16f1bfa5a69ad181f20244c014fa4
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-04 22:08:02 +0530

    All tests passed

--- L_3210.java
+++ L_3210.java
@@ -1,3 +1,11 @@
 public class L_3210 {
+    public String getEncryptedString(String s, int k) {
 
+        StringBuilder answer = new StringBuilder("");
+
+        for (int i = 0; i < s.length(); i++) {
+            answer = answer.append(s.charAt((i + k) % s.length()));
+        }
+        return answer.toString();
+    }
 }
