f9731859f2a8e7042b3032279f4014937d014078
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-08 22:07:55 +0530

    All tests passed

--- L_2475.java
+++ L_2475.java
@@ -1,3 +1,16 @@
 public class L_2475 {
 
+    public int unequalTriplets(int[] nums) {
+        int answer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if ((nums[i] != nums[j]) && (nums[j] != nums[k]) && (nums[i] != nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
