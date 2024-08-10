b63f46bed987816f0280f3b2d0b40ce2a5adaf10
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-10 23:36:04 +0530

    All tests passed

--- L_905.java
+++ L_905.java
@@ -1,3 +1,24 @@
-public class L_905 {
+import java.util.ArrayList;
 
+public class L_905 {
+    public int[] sortArrayByParity(int[] nums) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                arr.add(nums[i]);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                arr.add(nums[i]);
+            }
+        }
+        int[] ans = new int[arr.size()];
+
+        for (int i = 0; i < arr.size(); i++) {
+            ans[i] = arr.get(i);
+        }
+        return ans;
+    }
 }
