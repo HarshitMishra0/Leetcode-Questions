8c2bc80d546dc47f27a2521a4d591fa74e61deca
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-11 22:11:20 +0530

    All tests passed

--- L_1460.java
+++ L_1460.java
@@ -1,3 +1,14 @@
-public class L_1460 {
+import java.util.Arrays;
 
+public class L_1460 {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        for (int i = 0; i < target.length; i++) {
+            if (arr[i] != target[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
