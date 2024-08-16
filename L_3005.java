d11f29efb7e5d13f7644d643dac5cccf000c97c5
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-16 23:34:26 +0530

    All tests passed

--- L_3005.java
+++ L_3005.java
@@ -1,3 +1,30 @@
 public class L_3005 {
+    public int maxFrequencyElements(int[] nums) {
+        int arr[] = new int[nums.length];
 
+        for (int i = 0; i < nums.length; i++) {
+            int frequency = 1;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    frequency++;
+                }
+            }
+            arr[i] = frequency;
+        }
+        int max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int frequency = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                frequency = frequency + max;
+            }
+        }
+        return frequency;
+    }
 }
