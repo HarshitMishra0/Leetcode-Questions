162924f32f23b2df191b41484817b4a4127b6bf0
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-17 22:49:38 +0530

    All tests passed

--- L_1844.java
+++ L_1844.java
@@ -1,3 +1,16 @@
 public class L_1844 {
+    public String replaceDigits(String s) {
+        String b = "";
 
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                int a = (s.charAt(i - 1) - '0') + s.charAt(i);
+                b = b + (char) (a);
+            } else {
+                b = b + ch;
+            }
+        }
+        return b;
+    }
 }
