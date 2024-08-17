bcdd90732c0d3866001a69601682688f0062d314
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-17 22:47:59 +0530

    All tests passed

--- L_2053.java
+++ L_2053.java
@@ -1,3 +1,27 @@
-public class L_2053 {
+import java.util.ArrayList;
 
+public class L_2053 {
+    public String kthDistinct(String[] arr, int k) {
+        ArrayList<String> str = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                String a = arr[i];
+                String b = arr[j];
+                if (i != j && a.equals(b)) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                str.add(arr[i]);
+            }
+        }
+
+        if (str.size() < k) {
+            return "";
+        } else {
+            return str.get(k - 1);
+        }
+    }
 }
