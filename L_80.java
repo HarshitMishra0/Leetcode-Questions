d82eff31d0208479dedafc11adbb00d19a673ac8
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-20 23:43:03 +0530

    All tests passed

--- L_80.java
+++ L_80.java
@@ -1,3 +1,14 @@
 public class L_80 {
+    public int removeDuplicates(int[] nums) {
 
+        int k = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
 }
