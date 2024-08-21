51379706e47c2fe9e039c992c7cd05272f1973b8
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-21 08:56:33 +0530

    All tests passed

--- L_7.java
+++ L_7.java
@@ -1,3 +1,28 @@
 public class L_7 {
+    public int reverse(int x) {
+        int answer = 0;
 
+        if (x > 0) {
+            while (x > 0) {
+                int temp = x % 10;
+                if (answer < Integer.MIN_VALUE / 10 || answer > Integer.MAX_VALUE / 10) {
+                    return 0;
+                }
+                answer = answer * 10 + temp;
+
+                x = x / 10;
+            }
+        } else {
+            while (x < 0) {
+                int temp = x % 10;
+                if (answer < Integer.MIN_VALUE / 10 || answer > Integer.MAX_VALUE / 10) {
+                    return 0;
+                }
+                answer = answer * 10 + temp;
+
+                x = x / 10;
+            }
+        }
+        return answer;
+    }
 }
