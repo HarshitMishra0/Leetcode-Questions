fd54119efab47fb0c48f35bd7bb7ab5cf32fd012
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-25 23:58:03 +0530

    All tests passed

--- L_650.java
+++ L_650.java
@@ -1,3 +1,21 @@
-public class L_650 {
+import java.util.ArrayList;
 
+public class L_650 {
+    public int minSteps(int n) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                arr.add(i);
+                n /= i;
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            answer = answer + arr.get(i);
+        }
+        return answer;
+    }
 }
