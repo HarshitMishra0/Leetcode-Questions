34ad292b1d7dff5ddf7cb63f10b87a3056dcc928
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-28 22:12:57 +0530

    All tests passed

--- L_73.java
+++ L_73.java
@@ -1,3 +1,31 @@
 public class L_73 {
+    public void setZeroes(int[][] matrix) {
 
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int[][] arr = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 0) {
+                    int a = 0;
+                    int b = 0;
+                    while (a < arr[i].length) {
+                        matrix[i][a] = 0;
+                        a++;
+                    }
+                    while (b < arr.length) {
+                        matrix[b][j] = 0;
+                        b++;
+                    }
+                }
+            }
+        }
+    }
 }
