1546ac69bfd7578b6e1848cd3515215a24b7caf4
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-08-30 23:36:23 +0530

    All tests passed

--- L_238.java
+++ L_238.java
@@ -1,3 +1,17 @@
 public class L_238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
 
+        answer[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int a = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * a;
+            a = a * nums[i];
+        }
+        return answer;
+    }
 }
