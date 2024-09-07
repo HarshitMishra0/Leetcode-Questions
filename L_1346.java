de46abb3018429a53b9de7dc5043c348739c495e
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-09-07 18:46:07 +0530

    All tests passed

---
+++ L_1346.java
@@ -0,0 +1,13 @@
+public class L_1346 {
+    public boolean checkIfExist(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
