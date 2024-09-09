1307f0a27db291a512c7ff45ba54bdbda74ef3fc
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-09-09 07:31:29 +0530

    All tests passed

--- L_441.java
+++ L_441.java
@@ -1,3 +1,15 @@
 public class L_441 {
+    public int arrangeCoins(int n) {
 
+        int i = 1;
+        while (n > 0) {
+            n = n - i;
+            i++;
+        }
+
+        if (n < 0) {
+            return i - 2;
+        }
+        return i - 1;
+    }
 }
