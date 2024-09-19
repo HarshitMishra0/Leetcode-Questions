46b0cddbb70c0d6b5acc27d372e40462f0e50db7
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-09-19 23:09:34 +0530

    All tests Passed

--- L_1310.java
+++ L_1310.java
@@ -1,3 +1,31 @@
-public abstract class L_1310 {
+import java.util.HashMap;
 
+public abstract class L_1310 {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        ;
+
+        for (int i = 0; i < arr.length; i++) {
+            hm.put(i, arr[i]);
+        }
+
+        int answer[] = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int a = queries[i][0];
+            int b = queries[i][1];
+            if (a == b) {
+                answer[i] = arr[a];
+            } else {
+                int diff = b - a;
+                int k = arr[a];
+                for (int j = 1; j <= diff; j++) {
+                    k = k ^ arr[a + j];
+                }
+                answer[i] = k;
+            }
+
+        }
+
+        return answer;
+    }
 }
