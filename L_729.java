cf1587695e56dca53dc63219e23c0dd029e5d6f1
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-10-02 20:56:26 +0530

    All tests Passed

---
+++ L_729.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class L_729 {
+    private List<int[]> bookings;
+
+    public MyCalendar() {
+        bookings = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] event : bookings) {
+            int bookedStart = event[0];
+            int bookedEnd = event[1];
+
+            if (start < bookedEnd && end > bookedStart) {
+                return false;
+            }
+        }
+        bookings.add(new int[] { start, end });
+        return true;
+    }
+}
