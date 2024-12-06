a825e747ab5dfb043ab0bbd0969e94d546964431
Harshit Mishra <harshitmishra.inbox@gmail.com>
2024-12-06 08:41:29 +0530

    update

---
+++ L_1455.java
@@ -0,0 +1,13 @@
+public class L_1455 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+
+        String arr[] = sentence.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
