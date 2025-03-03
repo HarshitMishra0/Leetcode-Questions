61583c21afe6806bad1ee152ccbd1f9bbde12890
Harshit Mishra <harshitmishra.inbox@gmail.com>
2025-03-03 06:49:40 +0530

    All tests passed

---
+++ L_3264.java
@@ -0,0 +1,18 @@
+public class L_3264 {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+
+        while (k > 0) {
+            int min = nums[0];
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < min) {
+                    min = nums[i];
+                    index = i;
+                }
+            }
+            nums[index] = nums[index] * multiplier;
+            k--;
+        }
+        return nums;
+    }
+}
